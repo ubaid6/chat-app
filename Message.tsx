016aad9cf919bf1a8c8bff3f1539c821b64b6198
Ubaid Ahmed <ubaid.khan2@yahoo.com>
2022-08-22 19:37:29 +0100

    Message displayed based on author

--- Message.tsx
+++ Message.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, View } from "react-native";
 import { UserContext } from "./Context";
 import { channel } from "./stylesheet";
 
 
 const Message = (props: any): JSX.Element => {
+    const {user, setUser} = useContext(UserContext);
+    var author:boolean = false;
+
+    if (props.userId === user)
+        author = true;
+    else
+        author = false;
+
     return (
         <>
-            <View style={channel.author_message}>
+            <View style={author? channel.author_message : channel.other_message}>
                 <Text style={channel.message_text}>
                     {props.text}
                 </Text>
